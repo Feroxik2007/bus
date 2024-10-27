7191984f1917b7bd44bae7e25c87abba9e4eb1f7
Feroxik2007 <kovalikadrian@gmail.com>
2024-10-27 15:04:34 +0100

    Update main.java

--- main.java
+++ main.java
@@ -6,6 +6,7 @@ public class main {
         Scanner scanner = new Scanner(System.in);
         
         Integer [] miesta = {1, 2, 3 ,4 , 5, 6, 7, 8, 9, 10};
+        Integer [] rad = {1, 2 , 3, 4, 5};
         System.out.println("Welcome to our Bus");
         System.out.println("Would you like the enter we are going from Slovakia to Prague y/n");
         String answer = scanner.nextLine();
@@ -20,7 +21,6 @@ public class main {
         System.out.println("1. Buy Ticket, 2. Check available seats , 3. Exit");
         int buy = scanner.nextInt();
         
-       
             if(buy == 1){
                 buy();
             }
@@ -30,8 +30,17 @@ public class main {
             if(buy == 3){
                 exit();
             }
+            try {
+                FileWriter writer = new FileWriter("meno.txt");
+                
+                writer.write("Name:" + name+ "\n");
+                writer.write("Last Name:" + lastName+ "\n");
+                writer.write("\n");
+                writer.close();
+            } catch (Exception e) {
+                System.out.println("Something went wrong!");
+            }       
             
-              
         }
         
     }
@@ -43,45 +52,196 @@ public class main {
         int tickets = scanner.nextInt();
         System.out.println("You bought " + tickets + " tickets");
         System.out.println("Here is your seat number: " + randomIntBounded );
+           
 
     }
     public static void check(){
         Random rand = new Random();
         Scanner scanner = new Scanner(System.in);
+        double dph = 1;
         double price = 10.5;
         Integer [] miesta = {1, 2, 3 ,4 , 5, 6, 7, 8, 9, 10};
         int randomIntBounded = rand.nextInt(10);
-        System.out.println("These are the available seats");
-        System.out.println("1, 2, 3 ,4 , 5, 6, 7, 8, 9, 10");
-        System.out.println("Would you like to choose seat or our system can choose for you ? y/n");
-        String seat = scanner.nextLine();
-        if(seat.equals("y")){
-            System.out.println("What seat would you like to choose ?");
-            int seatNumber = scanner.nextInt();
-            System.out.println("You choosed seat number " + seatNumber);
-            System.out.println("one seat cost " + price + "€");
-            System.out.println("Here is your seat number:  " + seatNumber);  
-
-            try {
-                FileWriter writer = new FileWriter("ticket.txt");
-                writer.write("Seat number:" + seatNumber+ "\n");
-                writer.write("Price: " + price + "€" + "\n");
-                writer.write("\n");
-                writer.close();
-            } catch (Exception e) {
-                System.out.println("Something went wrong!");
-            }          
-        }
-        if (seat.equalsIgnoreCase("n")) {
-            System.out.println("Here is your ticket with your name and seat number: ");
-            System.out.println(" Seat number: " + randomIntBounded);
-        }
-       
+        int rows = rand.nextInt(5);
+            sigmmaaa();
+            payyy();
+            System.out.println("Your ticket will be sent to your email");
 
     }
     public static void exit(){
         System.out.println("Goodbye");
     }
-   
 
+   public static void payyy(){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("How would you like to pay? cash/card");
+    String payment = scanner.nextLine();
+    if (payment.equalsIgnoreCase("card")) {
+        System.out.println("You paid with card");
+        
+    }
+    else if(payment.equalsIgnoreCase("cash")){
+        System.out.println("Do you have exact change? y/n");
+        String change = scanner.nextLine();
+        if (change.equalsIgnoreCase("y")) {
+            System.out.println("You paid with cash");
+        }
+        else if (change.equalsIgnoreCase("n")) {
+            System.out.println("How much money you have?");
+            double money = scanner.nextDouble();
+            double price = 10.5;
+            if (money < price) {
+                System.out.println("You dont have enough money");
+            }
+            else if (money > price) {
+                double changee = money - price;
+                System.out.println("You paid with cash");
+                System.out.println("Here is your change: " + changee + "€");
+        }
+        
+        
+    }
+    else{
+        System.out.println("You have to choose between cash or card");
+    }
+   }
+  
+
+}
+    public static void sigmmaaa(){
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Here are the available seats and rows");
+        System.out.println("Row:1 , seats : 1, 2, 3");
+        System.out.println("Row:3 , seats : 7, 8, 9");
+        System.out.println("Row:4 , seats : 10, 11, 12");
+        System.out.println("Row:5 , seats : 13, 14, 15");
+        System.out.println("Would you like to choose seat/row or our system can choose for you ? y/n");
+        String seatt = scanner.nextLine();
+        if (seatt.equals("y")) {
+            System.out.println("What row do you want to choose?");
+        int row = scanner.nextInt();
+
+        if (row ==1) {
+            System.out.println("You choosed row 1");
+            System.out.println("What seat do you want to choose? 1 , 2 , 3");
+            int seat = scanner.nextInt();
+            if (seat == 1) {
+                System.out.println("You choosed seat 1");
+                System.out.println("And row 1");
+            }
+            if (seat == 2) {
+                System.out.println("You choosed seat 2");
+                System.out.println("And row 1");
+            }
+            if (seat == 3) {
+                System.out.println("You choosed seat 3");
+                System.out.println("And row 1");
+            }
+            if (seat > 3) {
+                System.out.println("There is no such seat");
+            }
+        }
+        if (row ==2) {
+            System.out.println("You choosed row 2");
+            System.out.println("What seat do you want to choose? 4 , 5 , 6");
+            int seat = scanner.nextInt();
+            if (seat == 4) {
+                System.out.println("You choosed seat 4");
+                System.out.println("And row 2");
+            }
+            if (seat == 5) {
+                System.out.println("You choosed seat 5");
+                System.out.println("And row 2");
+            }
+            if (seat == 6) {
+                System.out.println("You choosed seat 6");
+                System.out.println("And row 2");
+            }
+            if (seat >6) {
+                System.out.println("There is no such seat");
+            }
+        }
+        if (row ==3) {
+            System.out.println("You choosed row 3");
+            System.out.println("What seat do you want to choose? 7 , 8 , 9");
+            int seat = scanner.nextInt();
+            if (seat == 7) {
+                System.out.println("You choosed seat 7");
+                System.out.println("And row 3");
+            }
+            if (seat == 8) {
+                System.out.println("You choosed seat 8");
+                System.out.println("And row 3");
+            }
+            if (seat == 9) {
+                System.out.println("You choosed seat 9");
+                System.out.println("And row 3");
+            }
+            if (seat>9) {
+                System.out.println("There is no such seat");
+            }
+        }
+        if (row ==4) {
+            System.out.println("You choosed row 4");
+            System.out.println("What seat do you want to choose? 10 , 11 , 12");
+            int seat = scanner.nextInt();
+            if (seat == 10) {
+                System.out.println("You choosed seat 10");
+                System.out.println("And row 4");
+            }
+            if (seat == 11) {
+                System.out.println("You choosed seat 11");
+                System.out.println("And row 4");
+            }
+            if (seat == 12) {
+                System.out.println("You choosed seat 12");
+                System.out.println("And row 4");
+            }
+            if (seat > 12) {
+                System.out.println("There is no such seat");
+
+                
+            }
+        }
+        if (row ==5) {
+            System.out.println("You choosed row 5");
+            System.out.println("What seat do you want to choose? 13 , 14 , 15");
+            int seat = scanner.nextInt();
+            if (seat == 13) {
+                System.out.println("You choosed seat 13");
+                System.out.println("And row 5");
+
+            }
+            if (seat == 14) {
+                System.out.println("You choosed seat 14");
+                System.out.println("And row 5");
+            }
+            if (seat == 15) {
+                System.out.println("You choosed seat 15");
+                System.out.println("And row 5");
+            }
+            if (seat > 15) {
+                System.out.println("There is no such seat");
+                
+            }
+            
+        }
+        
+        try {
+            FileWriter writer = new FileWriter("ticket.txt");
+            writer.write("Row number:" + row+ "\n");
+            writer.write("\n");
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("Something went wrong!");
+        }        
+        }
+        if (seatt.equals("n")) {
+            System.out.println("Our system will choose for you");
+            int randomIntBounded = (int)(Math.random()*15);
+            System.out.println("Here is your seat number: " + randomIntBounded);
+        }
+        
+        
+    }
 }
